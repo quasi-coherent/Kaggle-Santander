b330637875de1a6313491db2034931783bf1fd56
Eric Pinkham <eric.a.pinkham@gmail.com>
2016-04-05 17:25:11 -0500

    Modified to use random forests instead of svms

--- santander_03.py
+++ santander_03.py
@@ -1,14 +1,14 @@
 import numpy as np
 import pandas as pd
-from sklearn import svm
+from sklearn.ensemble import RandomForestClassifier
 from sklearn.metrics import confusion_matrix
 from scipy.stats import mode
 
 """ A second take on this problem, """
 
-class hella_svms():
+class hella_rfs():
     def __init__(self):
-        self.svms = []
+        self.rfs = []
     def fit(self,train_df):
         #split df into zeros and ones dfs
         zeros = np.array(train_df[train_df.TARGET == 0].drop('TARGET',axis = 1))
@@ -20,21 +20,21 @@ class hella_svms():
         i = 0
         for chunk in chunks:
             print('training svm %s of %s' %(i,num_chunks))
-            chunk_svm = svm.LinearSVC()
+            chunk_rf = RandomForestClassifier(n_estimators = 50)
             chunk_ones = ones[np.random.rand(ones.shape[0])>0.5,:]
             chunk_train_X = np.concatenate([chunk,chunk_ones])
             chunk_train_Y = np.concatenate([np.zeros([chunk.shape[0],1]),np.ones([chunk_ones.shape[0],1])]).ravel()
-            chunk_svm.fit(chunk_train_X,chunk_train_Y)
-            self.svms.append(chunk_svm)
+            chunk_rf.fit(chunk_train_X,chunk_train_Y)
+            self.rfs.append(chunk_rf)
             i+=1
         #remove the last one, since it wont be as good as the rest
         #probably should just not train it in the first place
-        self.svms = self.svms[:-1]
+        self.rfs = self.rfs[:-1]
     def predict(self,test_df):
         test_X = np.array(test_df)
         predictions = []
-        for svm_i in self.svms:
-            predictions.append(svm_i.predict(test_X))
+        for rf_i in self.rfs:
+            predictions.append(rf_i.predict(test_X))
         predictions = np.array(predictions).T
         predictions = np.array(mode(predictions,axis = 1).mode).reshape([test_X.shape[0],1])
         return(predictions)
@@ -62,12 +62,12 @@ def cross_validate():
     test_df = pd.concat([zeros_test_df,ones_test_df])
     #return(test_df,train_df)
     #train
-    my_hella_svms = hella_svms()
-    my_hella_svms.fit(train_df)
+    my_hella_rfs = hella_rfs()
+    my_hella_rfs.fit(train_df)
     
     
     #predict
-    predictions = my_hella_svms.predict(test_df.drop('TARGET',axis = 1))
+    predictions = my_hella_rfs.predict(test_df.drop('TARGET',axis = 1))
     test_Y = np.array(test_df.TARGET) #true target values
     conf_matrix = confusion_matrix(test_Y,predictions)
     print('confusion matrix:')
@@ -91,18 +91,18 @@ def run_for_kaggle():
     #test_X = np.array(test_df)
     
     #train svm
-    my_hella_svms = hella_svms()
-    my_hella_svms.fit(train_df)
+    my_hella_rfs = hella_rfs()
+    my_hella_rfs.fit(train_df)
     
     #predict
-    predictions = my_hella_svms.predict(test_df).astype(int).ravel()
+    predictions = my_hella_rfs.predict(test_df).astype(int).ravel()
     
     #output
     out_df = pd.DataFrame(np.array([test_df.index,predictions]).T,columns = ['ID','TARGET'])
     out_df.to_csv('predictions.csv',index = False)
     
 if __name__ == '__main__':
-    cross_validate()
-    #run_for_kaggle()
+    #cross_validate()
+    run_for_kaggle()
     
     
